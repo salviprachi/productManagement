94ca7974a0d47b686da39277943004e17be6da9d
Salvi <prachi.salvi@accenture.com>
2021-04-12 23:29:33 +0530

    test cases added

--- src/app/component/item-inventory/item-inventory.component.spec.ts
+++ src/app/component/item-inventory/item-inventory.component.spec.ts
@@ -8,7 +8,8 @@ describe('ItemInventoryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ItemInventoryComponent ]
+      declarations: [ ItemInventoryComponent ],
+      imports :[]
     })
     .compileComponents();
   });
@@ -22,4 +23,101 @@ describe('ItemInventoryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create @itemInventoryClicked() for itemInventoryFlag', () => {
+     component.itemInventoryClicked()
+    expect(component.itemInventoryFlag).toBe(true);
+  });
+  it('should create @itemInventoryClicked() for addNewFlag', () => {
+     component.itemInventoryClicked()
+    expect(component.addNewFlag).toBe(false);
+  });
+  it('should create @itemInventoryClicked() for adminFlag', () => {
+    component.itemInventoryClicked()
+    expect(component.adminFlag).toBe(false);
+  });
+  it('should create @adminActionClicked() for itemInventoryFlag', () => {
+    component.adminActionClicked()
+   expect(component.itemInventoryFlag).toBe(false);
+ });
+ it('should create @adminActionClicked() for addNewFlag', () => {
+    component.adminActionClicked()
+   expect(component.addNewFlag).toBe(false);
+ });
+ it('should create @adminActionClicked() for adminFlag', () => {
+   component.adminActionClicked()
+   expect(component.adminFlag).toBe(true);
+ });
+ it('should create @editItem()', () => {
+  const item = {
+    item_name : "test",
+    item_image : "test1",
+    disconted_price : "test1",
+    actual_price : "test1",
+    description : "test2"
+
+  }
+  component.editItem(item)
+  expect(component.item_name).toBe("test");
+});
+
+it('should create @saveItem', () => {
+  const   items = [
+    {
+      item_id: 1,
+      item_name: "Tomato, 1kg",
+      item_image: "tomato.jpg",
+      disconted_price: "₹22.00",
+      actual_price: "₹61.00",
+      description: "fresh",
+    },
+    {
+      item_id: 4,
+      item_name: "Lemon, 250 g",
+      item_image: "lemon.jpg",
+      disconted_price: "₹36.00",
+      actual_price: "₹50.00",
+      description: "fresh",
+    }
+  ]
+  component.saveItem(items)
+  expect(component.copyofitems[3].item_name).toBe('Lemon, 250 g');
+});
+
+it('should create @deleteItem', () => {
+  const items = [
+    {
+      item_id: 1,
+      item_name: "Tomato, 1kg",
+      item_image: "tomato.jpg",
+      disconted_price: "₹22.00",
+      actual_price: "₹61.00",
+      description: "fresh",
+    }
+  ]
+  component.deleteItem(items)
+  expect(component.copyofitems[0].item_id).toBe(1);
+});
+
+it('should create @deleteItem', () => {
+  const items = [
+    {
+      item_id: 2,
+      item_name: "Tomato, 1kg",
+      item_image: "tomato.jpg",
+      disconted_price: "₹22.00",
+      actual_price: "₹61.00",
+      description: "fresh",
+    }
+  ]
+  component.deleteItem(items)
+  expect(component.copyofitems[1].item_id).toBe(2);
+});
+
+it('should create @addNewItem', () => {
+  component.addNewItem()
+  expect(component.addNewFlag).toBe(false);
+});
+
+  
 });
